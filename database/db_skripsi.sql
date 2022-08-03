e11e3398ec056efd72a409757884ec2377f9f310
riskymiradrachman <rizkymirad177@gmail.com>
2022-08-03 12:08:40 +0700

    commit message here

--- database/db_skripsi.sql
+++ database/db_skripsi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 30 Jul 2022 pada 18.36
+-- Waktu pembuatan: 03 Agu 2022 pada 07.07
 -- Versi server: 10.4.19-MariaDB
 -- Versi PHP: 7.3.28
 
@@ -48,8 +48,8 @@ CREATE TABLE `data_kost` (
 
 INSERT INTO `data_kost` (`id`, `nama`, `harga`, `fasilitas`, `alamat`, `no_telepon`, `lokasi`, `kost`, `kamar`, `toilet`, `kamar2`, `user_id`) VALUES
 (72, 'Asrama Tolaki', 'Rp. 450.000/Bulan', 'Kamar Tidur, WC, Dapur, Ruang Tamu', 'Jln. Kancil, Andonuhu', '082160565621', 'https://goo.gl/maps/EKVYCAtna3rfj8w47', 'Screenshot_(298).png', 'Screenshot_(299).png', 'Screenshot_(300).png', '10+_Best_Rick_and_Morty_Wallpapers_HD.png', 5),
-(73, 'Asrama Pink', 'Rp. 500.000/Bulan', 'Kamar Tidur, WC', 'Jln. Kancil, Andonuhu', '082160565621', 'https://goo.gl/maps/EKVYCAtna3rfj8w47', '10+_Best_Rick_and_Morty_Wallpapers_HD.png', 'A_Selection_of_Projects.jpg', 'bi-b.png', 'Uyoo.png', 23),
-(74, 'Kost Raka', 'Rp. 300.000/Bulan', 'Kamar Tidur, WC, Dapur, Ruang Tamu', 'Jln. Kancil, Andonuhu', '082160565621', 'https://goo.gl/maps/EKVYCAtna3rfj8w47', 'Live_gift.png', 'logo_genbi_id.png', 'logo_genbi_sultra.png', 'Muh_RiskyMirad.JPG', 22);
+(74, 'Kost Raka', 'Rp. 300.000/Bulan', 'Kamar Tidur, WC, Dapur, Ruang Tamu', 'Jln. Kancil, Andonuhu', '082160565621', 'https://goo.gl/maps/EKVYCAtna3rfj8w47', 'Live_gift.png', 'logo_genbi_id.png', 'logo_genbi_sultra.png', 'Muh_RiskyMirad.JPG', 22),
+(75, 'Kost Cinta', 'Rp. 300.000/Bulan', 'Kamar Tidur, WC, Dapur, Ruang Tamu', 'Jln. Kancil, Andonuhu', '081234567890', 'https://goo.gl/maps/EKVYCAtna3rfj8w47', 'A_Selection_of_Projects.jpg', '10+_Best_Rick_and_Morty_Wallpapers_HD.png', 'Live_gift.png', 'Uyoo1.png', 34);
 
 -- --------------------------------------------------------
 
@@ -73,11 +73,11 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
-(5, 'Iin Juliana Budiman', 'iinju.budiman@gmail.com', 'gambar.jpeg', '$2y$10$xWuosZyn76gIkbz5XXPdJ.31H9Pfqr1SHqJ.UvASD8ALqwGkw5ez2', 1, 1, 1655965894),
-(6, 'Risky Manguluang', 'risky@gmail.com', '1.png', '$2y$10$JvJN0MO8FJ1shLg2hGZFHeKJsX742caHggv23txdlCD1ScqbehOUK', 2, 1, 1655972323),
+(5, 'Iin Juliana Budiman', 'iinju.budiman@gmail.com', 'gambar.jpeg', '$2y$10$xWuosZyn76gIkbz5XXPdJ.31H9Pfqr1SHqJ.UvASD8ALqwGkw5ez2', 2, 1, 1655965894),
 (21, 'Angela Stevani', 'angela@gmail.com', '1.png', '$2y$10$KgT/Gukix8nBgM67cnwDAOUU6yVyKf.RNt/aTS8yQlt88xStC0cZ2', 1, 1, 1658132099),
 (22, 'Abdul Mulqi Rachman', 'mulqi@gmail.com', '1.png', '$2y$10$I7HtyviGCNthKwqok2c7tOSwH4GFbljehhtKM8YAzwVYe7clL3/B2', 2, 1, 1659190668),
-(23, 'Muhammad Risky Mirad Rachman', 'rizkymirad177@gmail.com', 'Muh_RiskyMirad.JPG', '$2y$10$TesKFjQKf7BR38mCXHQ1TuNvZtCV2YZJYhi3H/Z/rrRcCijfVqb1C', 2, 1, 1659194217);
+(33, 'Muhammad Risky Mirad Rachman', 'rizkymirad177@gmail.com', 'Muh_RiskyMirad1.JPG', '$2y$10$nDt0pdkPTIrVG5X4XxJwP.EuXJhGTvNMaWtKhHHNZLmyw/geN5Ma.', 1, 1, 1659358594),
+(34, 'Alfian Izzah', 'alfian@gmail.com', '1.png', '$2y$10$FkwxgaR4bJK1k9n9wSiR4ugYHeXo2fdMi3WxQlVXE9hE3elvFwfaO', 2, 1, 1659361581);
 
 -- --------------------------------------------------------
 
@@ -169,6 +169,27 @@ INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active
 (6, 1, 'Manajemen Akun', 'admin/editakun', 'fas fa-fw fa-user', 1),
 (10, 2, 'Ubah Password', 'user/ubahpassword', 'fas fa-fw fa-key', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_token`
+--
+
+CREATE TABLE `user_token` (
+  `id` int(11) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `token` varchar(128) NOT NULL,
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user_token`
+--
+
+INSERT INTO `user_token` (`id`, `email`, `token`, `date_created`) VALUES
+(32, 'rizkymirad177@gmail.com', 'pZKNP8WvkodJ9jW5IqefDbkYe06usqFpiDQQyWbrmS8=', 1659360797),
+(33, 'alfian@gmail.com', 'AHhrgcWTlhMpT8jkf5RwUvNfPr3y1NtARmnYyo4fphI=', 1659361581);
+
 --
 -- Indexes for dumped tables
 --
@@ -211,6 +232,12 @@ ALTER TABLE `user_sub_menu`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `user_token`
+--
+ALTER TABLE `user_token`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -218,13 +245,13 @@ ALTER TABLE `user_sub_menu`
 -- AUTO_INCREMENT untuk tabel `data_kost`
 --
 ALTER TABLE `data_kost`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_access_menu`
@@ -251,6 +278,12 @@ ALTER TABLE `user_sub_menu`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
+-- AUTO_INCREMENT untuk tabel `user_token`
+--
+ALTER TABLE `user_token`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
@@ -258,7 +291,7 @@ ALTER TABLE `user_sub_menu`
 -- Ketidakleluasaan untuk tabel `data_kost`
 --
 ALTER TABLE `data_kost`
-  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
